2a9c9fbe495481c560578da249bb7de282026723
Alexey Loktev <a.a.loktev@yandex.ru>
2022-06-28 22:04:22 +0300

    Выполнил дополнительное задание 22 урока

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,23 +1,19 @@
 "use strict";
 const menu = () => {
-  const menuBtn = document.querySelector(".menu");
   const menu = document.querySelector("menu");
+  const body = document.querySelector("body");
 
-  const handleMenu = () => {
-    menu.classList.toggle("active-menu");
-  };
-
-  menuBtn.addEventListener("click", handleMenu);
-
-  menu.addEventListener("click", (event) => {
-    let target = event.target;
-    if (target.classList.contains("close-btn")) {
-      handleMenu();
-    } else if (target.nodeName === "A") {
-      handleMenu();
+  body.addEventListener("click", (e) => {
+    if (e.target.closest(".menu")) {
+      menu.classList.add("active-menu");
+    } else if (
+      !e.target.closest("menu") ||
+      e.target.classList.contains("close-btn") ||
+      e.target.closest("a")
+    ) {
+      menu.classList.remove("active-menu");
     }
   });
-  menuBtn.addEventListener("click", handleMenu);
 };
 
 export default menu;
